67e57624cd311393cc5340ec1c2db7df609bfdef
debuqer <bagher2g@gmail.com>
2022-08-26 00:12:52 +0430

    upload file using

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -27,7 +30,9 @@ func main() {
 func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	data := struct {
 		Username string
+		Name     string
 	}{
+		"",
 		"",
 	}
 
@@ -37,8 +42,32 @@ func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func applyLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	data := struct {
 		Username string
+		Name     string
 	}{
 		r.FormValue("username"),
+		r.FormValue("name"),
+	}
+
+	f, h, err := r.FormFile("profile")
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, "File is not readable", http.StatusBadGateway)
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile(filepath.Join("uploads/", h.Filename), bs, 0644)
+	if err != nil {
+		http.Error(w, "bad uploaded", http.StatusInternalServerError)
+		fmt.Println(err)
+		return
 	}
 
 	tpl.ExecuteTemplate(w, "login.gohtml", data)
